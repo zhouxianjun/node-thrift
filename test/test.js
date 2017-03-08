2a79053b7afd4d2934f46e0d9e5dad537f99a3ca
zxjun <715213471@qq.com>
2017-03-08 17:42:19 +0800

    init zk

--- test/test.js
+++ test/test.js
@@ -7,29 +7,3 @@ let client = nodeThrift.zk.createClient('127.0.0.1:2181');
 client.connect();
 let server = new nodeThrift.ServerRegister(client);
 server.load('./service/');
-
-/*const co = require('co');
-const thunkify = require('thunkify');
-let gen = function *(name) {
-    console.log(name);
-    return yield thunkify(gen2).apply(gen2, [name]);
-};
-let gen2 = (name, cb) => {
-    console.log('bbb');
-    cb(null, 'ok');
-};
-let th = (fn) => {
-    console.log(2);
-    return fn();
-};
-let t = () => {
-    th(async() => {
-        try {
-            let result = await co(gen.bind(co), 'Alone');
-            console.log(result);
-        } catch (err) {
-            console.log(err);
-        }
-    });
-};
-t();*/
