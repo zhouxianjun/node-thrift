e43eceb4dac6ccccd8bfe54b9bca88c2f43d8c3e
zxjun <715213471@qq.com>
2017-03-14 18:05:31 +0800

    update ReferenceBean

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports.ServerRegister = require('./lib/ZookeeperThriftServerRegister');
 exports.zk = require('node-zookeeper-client');
 exports.ServerProvider = require('./lib/ZookeeperThriftServerProvider');
 exports.invoker = {
+    invoker: require('./lib/invoker/Invoker'),
     AbstractInvoker: require('./lib/invoker/AbstractInvoker'),
     PoolInvoker: require('./lib/invoker/PoolInvoker'),
     factory: {
@@ -13,7 +14,13 @@ exports.invoker = {
     }
 };
 exports.loadBalance = {
+    loadBalance: require('./lib/loadbalance/LoadBalance'),
     AbstractLoadBalance: require('./lib/loadbalance/AbstractLoadBalance'),
     RandomLoadBalance: require('./lib/loadbalance/RandomLoadBalance'),
     RoundRobinLoadBalance: require('./lib/loadbalance/RoundRobinLoadBalance')
+};
+exports.router = require('./lib/router/Router');
+exports.filter = {
+    filter: require('./lib/filter/Filter'),
+    BasicFilter: require('./lib/filter/BasicFilter')
 };
