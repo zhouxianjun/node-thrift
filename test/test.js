152f8970767f7a28c80b23fe51ceb19d252fe279
zhouxianjun <zxj19910611>
2017-03-24 20:03:50 +0800

    add attr

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@
  * Created by Alone on 2017/3/8.
  */
 'use strict';
-const V = require('../lib/ZookeeperThriftServerProvider');
-let v = new V();
-v.on('ready', () => {
-    console.log('ok');
-});
-v.load();
+const merge = require('merge');
+console.log(merge({
+    array: [11]
+}, {
+    array: [22]
+}));
